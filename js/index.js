77dfcc5053bf96bad128d1159813c09a1754f251
Ryan Smith <ryan.l.smith142@gmail.com>
2019-10-22 08:32:23 -0500

    finish project set up, add mapbox functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 "use strict";
+
+//Creates a map and centers it on San Antonio,TX
+mapboxgl.accessToken = mapboxToken;
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v9',
+    zoom: 9,
+    center: [-98.4936, 29.4241]
+}); //mapboxgl object
+
+document.getElementById('map').innerHTML = map;
+
+
+//Creates draggable marker
+var marker = new mapboxgl.Marker({
+    draggable: true
+}) //marker object
+    .setLngLat([-98.4936, 29.4241])
+    .addTo(map);
+
+map.addControl(new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken
+    // mapboxgl: mapboxgl
+}));
